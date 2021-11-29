b75a8af7c48bad2c92ff297beb7447b8dc57eede
DELL <DELL@DESKTOP-J4MJFQK>
2021-11-29 16:14:47 +0530

    ADDED TEST7 CLASS

---
+++ src/test/java/Selenium/Automation/Test7.java
@@ -0,0 +1,8 @@
+package Selenium.Automation;
+
+public class Test7 {
+	public static void main(String[] args) throws InterruptedException {
+	
+	System.out.println("Im modififying from local as a tester");
+	}
+}
