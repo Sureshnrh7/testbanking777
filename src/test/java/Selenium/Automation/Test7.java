6c9c4e8e8a6a9dd62e03dcc3d19af3b287d0e0a1
DELL <DELL@DESKTOP-J4MJFQK>
2021-11-29 16:42:38 +0530

    Added one more console message

--- src/test/java/Selenium/Automation/Test7.java
+++ src/test/java/Selenium/Automation/Test7.java
@@ -4,5 +4,7 @@ public class Test7 {
 	public static void main(String[] args) throws InterruptedException {
 	
 	System.out.println("Im modififying from local as a tester");
+	System.out.println("added one more satetement  from local as a tester");
+
 	}
 }
